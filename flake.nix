7cae43d0419ee1f90780dceea3f9098fb2d82842
PhilVoel <philipp123.voeller@gmail.com>
2024-07-09 15:48:09 +0200

    Add shell flake and direnv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+	description = "Zitate-Bot shell flake";
+	inputs.nixpkgs.url = "github:nixos/nixpkgs/24.05";
+
+	outputs = {nixpkgs, ...}:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages."${system}".pkgs;
+	in {
+		devShells."${system}".default = pkgs.mkShell {
+			name = "Zitate-Bot shell flake";
+			buildInputs = with pkgs; [
+				bacon
+				cargo
+				clang
+				mold
+				rustc
+				sccache
+			];
+		};
+	};
+}
