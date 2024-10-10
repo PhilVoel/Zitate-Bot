8455c983c0af94ffd38bd3c0c19c380d40970236
PhilVoel <philipp123.voeller@gmail.com>
2024-10-10 04:41:55 +0200

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,26 @@
 {
 	description = "Zitate-Bot shell flake";
-	inputs.nixpkgs.url = "github:nixos/nixpkgs/24.05";
 
-	outputs = {nixpkgs, ...}:
-	let
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages."${system}".pkgs;
-	in {
-		devShells."${system}".default = pkgs.mkShell {
-			name = "Zitate-Bot shell flake";
-			packages = with pkgs; [
-				bacon
-				cargo
-				clang
-				mold
-				rustc
-				sccache
-			];
-		};
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/24.05";
+		flake-utils.url = "github:numtide/flake-utils/v1.0.0";
 	};
+
+	outputs = {flake-utils, nixpkgs, ...}:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = nixpkgs.legacyPackages."${system}";
+			in {
+				devShells.default = pkgs.mkShell {
+					name = "Zitate-Bot shell flake";
+					packages = with pkgs; [
+						bacon
+						cargo
+						clang
+						mold
+						rustc
+						sccache
+					];
+				};
+			}
+		);
 }
