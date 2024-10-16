de8e6bb11c03bea9e162bd33289b467c63891d08
PhilVoel <philipp123.voeller@gmail.com>
2024-10-16 22:41:00 +0200

    flake.nix

--- flake.nix
+++ flake.nix
@@ -6,20 +6,29 @@
 		flake-utils.url = "github:numtide/flake-utils/v1.0.0";
 	};
 
-	outputs = {flake-utils, nixpkgs, ...}:
+	outputs = {flake-utils, nixpkgs, self, ...}:
 		flake-utils.lib.eachDefaultSystem (system:
 			let pkgs = nixpkgs.legacyPackages."${system}";
 			in {
 				devShells.default = pkgs.mkShell {
 					name = "Zitate-Bot shell flake";
 					packages = with pkgs; [
-						bacon
 						cargo
-						clang
-						mold
-						rustc
-						sccache
 					];
+				};
+				packages.default = pkgs.rustPlatform.buildRustPackage {
+					pname = "zitate_bot";
+					version = "0.1.1";
+					src = self;
+					cargoLock.lockFile = ./Cargo.lock;
+					buildType = "debug";
+					doCheck = false;
+				};
+				packages.release = pkgs.rustPlatform.buildRustPackage {
+					pname = "zitate_bot";
+					version = "0.1.1";
+					src = self;
+					cargoLock.lockFile = ./Cargo.lock;
 				};
 			}
 		);
